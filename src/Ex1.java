09023dc8386ea99187534028dacf73a8ab08a21f
Maria Raquel <mdella@fiemg.com.br>
2024-11-05 09:47:54 -0300

    atividade

---
+++ src/Ex1.java
@@ -0,0 +1,14 @@
+/*Tem-se um conjunto de dados contendo a altura e o sexo (masculino, feminino) 
+ * de 10 pessoas. Fazer um algoritmo que calcule e escreva:
+a. a maior e a menor altura do grupo;
+b. média de altura dos homens;
+c. o número de mulheres.
+ */
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Ex2.java
@@ -0,0 +1,11 @@
+/*Criar um programa que calcule a média de salários de uma empresa, 
+ * pedindo ao usuário a grade de funcionários e os salários, e devolvendo a média salarial.
+ */
+public class Ex2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
